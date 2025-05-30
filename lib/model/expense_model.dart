2270d47f7c06be9aad5a45557dc1a6f9d103aa13
sid bhosale <bhosalesid777@gmail.com>
2025-05-30 23:24:30 +0530

    added expense_item.dart file

--- lib/model/expense_model.dart
+++ lib/model/expense_model.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  
+}
 
 class ExpenseModel {
   ExpenseModel({required this.title, required this.amount, required this.date, required this.category })
