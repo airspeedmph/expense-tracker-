603aeeb2908f2802ab1a2709b77328f326ac9f05
sid bhosale <bhosalesid777@gmail.com>
2025-05-30 23:28:09 +0530

    mapped enum to icons

--- lib/model/expense_model.dart
+++ lib/model/expense_model.dart
@@ -6,12 +6,20 @@ const uuid = Uuid();
 enum Category { food, travel, leisure, work }
 
 const categoryIcons = {
-  
-}
+  Category.food : Icon(Icons.lunch_dining),
+  Category.leisure : Icon(Icons.movie),
+  Category.work : Icon(Icons.work),
+  Category.travel : Icon(Icons.flight_takeoff),
+
+  };
 
 class ExpenseModel {
-  ExpenseModel({required this.title, required this.amount, required this.date, required this.category })
-    : id = uuid.v4();
+  ExpenseModel({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
 
   final String title;
   final String id;
